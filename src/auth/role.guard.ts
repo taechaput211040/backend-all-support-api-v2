1ead5cfb872099ebb185d8a26d7c6ab2e0b686e0
richard <richard.realtech@gmail.com>
2023-04-12 15:33:33 +0700

    add role guard

---
+++ src/auth/role.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { ROLES_KEY } from './auth.public';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    console.log('Role Guard');
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [context.getHandler(), context.getClass()]);
+    if (!requiredRoles) {
+      return true;
+    }
+    const { user } = context.switchToHttp().getRequest();
+    return requiredRoles.some((role) => user.roles?.includes(role));
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
