464ab35a42ff97eeea6eff9f7ab4c2982b88d363
richard <richard.realtech@gmail.com>
2023-04-12 15:41:45 +0700

    update role guard remove jwtservice

--- src/auth/role.guard.ts
+++ src/auth/role.guard.ts
@@ -6,7 +6,7 @@ import { ROLES_KEY } from './auth.public';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+  constructor(private reflector: Reflector) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     console.log('Role Guard');
     const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [context.getHandler(), context.getClass()]);
@@ -15,10 +15,5 @@ export class RoleGuard implements CanActivate {
     }
     const { user } = context.switchToHttp().getRequest();
     return requiredRoles.some((role) => user.roles?.includes(role));
-  }
-
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
   }
 }
